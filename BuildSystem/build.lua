e928b447ec25757676ce829cb1b34b78c33a4b8b
Francois-Le <98267232+Francois-Le@users.noreply.github.com>
2022-10-23 17:19:49 +0100

    Create build.lua

---
+++ BuildSystem/build.lua
@@ -0,0 +1,33 @@
+function bimg_include()
+    local baseFolder = debug.getinfo(1,'S').source:match("^@(.+)/buildSystem/build.lua$")
+
+    includedirs { path.join(baseFolder, "include") }
+end
+
+function bimg_project()
+    local baseFolder = debug.getinfo(1,'S').source:match("^@(.+)/buildSystem/build.lua$")
+
+    project "bimg"
+        kind "StaticLib"
+        language "C++"
+        cppdialect "C++14"
+        targetdir "%{wks.location}/bin/%{cfg.buildcfg}/%{prj.name}"
+        location "%{wks.location}/%{prj.name}"
+        exceptionhandling "Off"
+        rtti "Off"
+        files
+        {
+            path.join(baseFolder, "include/bimg/*.h"),
+            path.join(baseFolder, "src/image.cpp"),
+            path.join(baseFolder, "src/image_gnf.cpp"),
+            path.join(baseFolder, "src/*.h"),
+            path.join(baseFolder, "3rdparty/astc-codec/src/decoder/*.cc")
+        }
+        includedirs
+        {
+            path.join(baseFolder, "include"),
+            path.join(baseFolder, "3rdparty/astc-codec"),
+            path.join(baseFolder, "3rdparty/astc-codec/include"),
+        }
+        bx_include()
+end
